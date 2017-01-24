17c5aa166d1dbc79d9a68d0135ab84eb020d0627
keldlundgaard <keld.lundgaard@gmail.com>
2017-01-24 11:56:13 -0800

    adding support for defining several anharmonic modes in same analysis

---
+++ examples/multiple_anharmonic_modes/multiple_modes_vib_trans.py
@@ -0,0 +1,40 @@
+import sys
+
+from ase.structure import molecule
+from ase.lattice.surface import fcc111, add_adsorbate
+from ase.optimize import QuasiNewton
+from ase.constraints import FixAtoms
+from ase.calculators.emt import EMT
+from ase.vibrations import Vibrations
+
+sys.path.append("../..")
+
+from __init__ import AnharmonicModes
+
+slab = fcc111('Au', size=(2, 2, 2), vacuum=4.0)
+H = molecule('H')
+add_adsorbate(slab, H, 3.0, 'ontop')
+
+constraint = FixAtoms(mask=[a.symbol == 'Au' for a in slab])
+slab.set_constraint(constraint)
+
+slab.set_calculator(EMT())
+
+QuasiNewton(slab).run(fmax=0.05)
+
+vib = Vibrations(slab, indices=[8])
+vib.run()
+vib.summary()
+vib.clean()
+
+AM = AnharmonicModes(
+    vibrations_object=vib,
+    settings={'plot_mode': True})
+
+AM.define_vibration(mode_number=-1)
+AM.define_translation(from_atom_to_atom=[4, 6])
+
+# AM.inspect_anmodes()  # creates trajectory file
+AM.run()
+AM.summary()
+AM.clean()
