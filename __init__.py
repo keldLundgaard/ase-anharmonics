Fixed bug in entropy calculation and ZPE calculation

__init__.py
@@ -1,5 +1,5 @@
 """Anharmonic modes module for ASE
-
+#KDW
 Docs follows Google's python styling guide:
 http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
 
@@ -329,16 +329,22 @@ class AnharmonicModes:
         self.ZPE_modes = []  # Zero point energy for mode
         self.Z_modes = []  # partition function for mode
         self.e_exitation_modes = []  # principle energy of mode
+        self.entropic_energy_modes = [] #entropy contribution per vibrational mode
+        x = 0.
+        y = 0.
 
         for i, an_mode in enumerate(self.an_modes):
             # Partition function of mode
             Z_mode = 0.
+            y = 0.
             for ei in an_mode['energy_levels']:
                 Z_mode += np.exp(-(ei-an_mode['ZPE'])/self.kT)
+                y += np.exp(-(ei-an_mode['ZPE'])/self.kT) * (ei-an_mode['ZPE'])
+
+            self.entropic_energy_modes.append(self.kT * np.log(Z_mode) + 1./Z_mode * y)
 
             # Difference between ZPE and first excited energy level
-            e_min_exitation = (
-                an_mode['energy_levels'][1]-an_mode['energy_levels'][0])
+            e_min_exitation = (an_mode['energy_levels'][1]-an_mode['energy_levels'][0])
 
             self.ZPE_modes.append(an_mode['ZPE'])
             self.Z_modes.append(Z_mode)
@@ -350,17 +356,22 @@ class AnharmonicModes:
                 Z_mode = 1./(1.-np.exp(-e.real/(self.kT)))
                 ZPE = 0.5 * e.real
                 e_min_exitation = e.real
+                x = e_min_exitation/self.kT
+                self.entropic_energy_modes.append(self.kT*(x/(np.exp(x)-1.) - np.log(1.-np.exp(-x))))
+
             else:
                 ZPE = 0.
                 Z_mode = 1.
                 e_min_exitation = 0.
+                self.entropic_energy_modes.append(0.)
 
+            x = 0.
             self.ZPE_modes.append(ZPE)
             self.Z_modes.append(Z_mode)
             self.e_exitation_modes.append(e_min_exitation)
 
         # Calculating the entropic energy for each mode
-        self.entropic_energy_modes = -1*self.kT*np.log(self.Z_modes)
+        #self.entropic_energy_modes = -1*self.kT*np.log(self.Z_modes)
 
         # Overall information
         self.ZPE = np.sum(self.ZPE_modes)
@@ -376,21 +387,29 @@ class AnharmonicModes:
         ZPE_hmodes = []  # Zero point energy for mode
         Z_hmodes = []  # partition function for mode
         e_excitation_hmodes = []  # principle energy of mode
+        entropic_energy_hmodes = [] #entropy contribution per mode
+        x = 0.
 
         for e in hnu:
             if e.imag == 0 and e.real >= 0.010:
                 Z_mode = 1./(1.-np.exp(-e.real/(self.kT)))
                 ZPE = 0.5 * e.real
                 e_min_exitation = e.real
+                x = e_min_exitation/self.kT
+                entropic_energy_hmodes.append(self.kT*(x/(np.exp(x)-1.) - np.log(1.-np.exp(-x))))
+
             else:
                 ZPE = 0.
                 Z_mode = 1.
                 e_min_exitation = 0.
+                entropic_energy_hmodes.append(0.)
             ZPE_hmodes.append(ZPE)
             Z_hmodes.append(Z_mode)
             e_excitation_hmodes.append(e_min_exitation)
+            x = 0.
 
-        entropic_energy_hmodes = -1*self.kT*np.log(Z_hmodes)
+        #entropic_energy_hmodes = -1*self.kT*np.log(Z_hmodes)
+        
         entropic_energy = np.sum(entropic_energy_hmodes)
         ZPE_h = np.sum(ZPE_hmodes)
 

anh_base.py
@@ -119,7 +119,8 @@ class BaseAnalysis(object):
             xmin = 0.
             xmax = xmin+2.*np.pi/self.an_mode['symnumber']
 
-            groundstate_energy = self.an_mode['displacement_energies'][0]
+            #groundstate_energy = self.an_mode['displacement_energies'][0]
+            groundstate_energy = min(self.an_mode['displacement_energies'])
 
         elif self.an_mode['type'] == 'vibration':
             Hcoeff = units._hbar**2 / (2.*units._amu * units._e * 1e-20)
@@ -127,6 +128,7 @@ class BaseAnalysis(object):
             xmin = np.min(self.an_mode['displacements'])
             xmax = np.max(self.an_mode['displacements'])
 
+            #groundstate_energy = self.an_mode['displacement_energies'][0]
             groundstate_energy = self.an_mode['displacement_energies'][0]
 
         elif self.an_mode['type'] == 'translation':
@@ -136,7 +138,9 @@ class BaseAnalysis(object):
 
             Hcoeff = units._hbar**2/(2*units._amu * units._e * 1e-20)
 
+            #groundstate_energy = self.an_mode['displacement_energies'][0]
             groundstate_energy = self.an_mode['displacement_energies'][0]
+
         else:
             raise ValueError("No other types are currently supported")
 